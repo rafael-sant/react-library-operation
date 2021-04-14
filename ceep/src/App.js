ed97ec5f96ac8d6e7fb09026fdef6072e2062137
Rafael Barros <rafael.barros@netzee.com.br>
2021-04-14 07:49:01 -0300

    Transmissão de informações entre objetos, fluxo de atualização do sistema, para que serve o state, como atribuir o state de maneira segura, atualizando a visualizão da tela dinamicamente

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -5,15 +5,33 @@ import FormmularioCadastro from "./components/FomularioCadastro";
 import ListaDeNotas from './components/ListaDeNotas';
 
 class App extends Component {
-    render() {
-        return ( 
-          <section className="conteudo">
-            <FormmularioCadastro/>
-            <ListaDeNotas></ListaDeNotas>
-          </section >
 
-        );
+  constructor() {
+    super();
+    
+    this.state = {
+      notas: []
     }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto}
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render() {
+      return ( 
+        <section className="conteudo">
+          <FormmularioCadastro criarNota={this.criarNota.bind(this)}/>
+          <ListaDeNotas notas={this.state.notas}/>
+        </section >
+
+      );
+  }
 }
 
 export default App;
